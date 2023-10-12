fac65e53f2c3caf8a6e01b2c645ce2d47f07009a
Adam Howard <ahoward0920@gmail.com>
2023-10-12 09:47:16 -0400

    Forgot the URLs again

--- game_server/urls.py
+++ game_server/urls.py
@@ -4,12 +4,19 @@
 """
 from django.urls import path
 
-from .views import EntityCreateView, EntityView, UserCreateView, UserAuthView
+from .views import (
+    EntityCreateView,
+    EntitySearchView,
+    EntityView,
+    UserCreateView,
+    UserAuthView
+)
 
 app_name = "game_server"
 
 urlpatterns = [
     path("entity/add/", EntityCreateView.as_view(), name="entity-add"),
+    path("entity/search/", EntitySearchView.as_view(), name="entity-search"),
     path("entity/<uuid>/", EntityView.as_view(), name="entity"),
     path("user/add/", UserCreateView.as_view(), name="user-add"),
     path("user/auth/", UserAuthView.as_view(), name="user-auth"),
