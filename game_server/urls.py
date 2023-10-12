59e90034de174b2c3efeb9b4f956e113bd418577
Adam Howard <ahoward0920@gmail.com>
2023-10-11 23:16:38 -0400

    Setting up the urlconf

--- game_server/urls.py
+++ game_server/urls.py
@@ -4,6 +4,13 @@
 """
 from django.urls import path
 
+from .views import EntityCreateView, EntityView, UserCreateView, UserAuthView
+
 app_name = "game_server"
 
-urlpatterns = []
+urlpatterns = [
+    path("entity/add/", EntityCreateView.as_view(), name="entity-add"),
+    path("entity/<uuid>/", EntityView.as_view(), name="entity"),
+    path("user/add/", UserCreateView.as_view(), name="user-add"),
+    path("user/auth/", UserAuthView.as_view(), name="user-auth"),
+]
