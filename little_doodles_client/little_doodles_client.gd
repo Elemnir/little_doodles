8c57c9475e38425b763b712264680b95d9f5c0e2
Adam Howard <ahoward0920@gmail.com>
2023-10-13 13:25:56 -0400

    Finishing the first draft of the client API

--- little_doodles_client/little_doodles_client.gd
+++ little_doodles_client/little_doodles_client.gd
@@ -1,37 +1,71 @@
 class_name LittleDoodlesClient extends HTTPRequest
 
-var scheme = "https"
-var domain = "oatestbed.adamphoward.com/game/"
+@export var scheme = "https"
+@export var domain = "example.com/game/"
 var api_url = "%s://%s" % [scheme, domain]
 var cookiejar = {}
 
+const Endpoints = {
+	USER_AUTH = "user/auth/",
+	USER_CREATE = "user/create/",
+	ENTITY_SEARCH = "entity/search/?%s",
+	ENTITY_CREATE = "entity/add/",
+	ENTITY_GET = "entity/%s/"
+}
+
 
 func create_user(username, password):
-	pass
+	# Register a new user account with the server. Also logs the user in.
+	# Returns true if successful
+	var resp = await _send_request(Endpoints.USER_CREATE)
+	if resp == null:
+		return false
+	resp = await _send_request(
+		Endpoints.USER_CREATE, 
+		HTTPClient.METHOD_POST,
+		JSON.stringify({
+			"csrfmiddlewaretoken": resp["csrf_token"],
+			"username": username,
+			"password1": password,
+			"password2": password,
+		})
+	)
+	return resp != null
 
 
 func auth_user(username, password):
-	pass
+	# Log an existing user into the server. Returns true is successful
+	var resp = await _send_request(Endpoints.USER_AUTH)
+	if resp == null:
+		return false
+	resp = await _send_request(
+		Endpoints.USER_AUTH, 
+		HTTPClient.METHOD_POST,
+		JSON.stringify({
+			"csrfmiddlewaretoken": resp["csrf_token"],
+			"username": username,
+			"password": password,
+		})
+	)
+	return resp != null
 
 
-func save_entity(entity):
+func save_entity(entity) -> bool:
 	# Create the Entity on the server if it doesn't already have a UUID,
 	# otherwise update it. Returns true if the save completed successfully
-	var endpoint = "entity/add/" if entity.uuid == null else "entity/%s/" % entity.uuid
+	var endpoint = Endpoints.ENTITY_CREATE if entity.uuid == null else Endpoints.ENTITY_GET % entity.uuid
 	var resp = await _send_request(endpoint)
 	if resp == null:
 		return false
 	resp = await _send_request(
 		endpoint, HTTPClient.METHOD_POST, entity.as_request_body(resp["csrf_token"])
 	)
-	if resp == null:
-		return false
-	return true
+	return resp != null
 
 
 func get_entity(uuid):
 	# Returns an Entity instance selected via UUID.
-	var resp = await _send_request("entity/%s/" % uuid)
+	var resp = await _send_request(Endpoints.ENTITY_GET % uuid)
 	if resp == null:
 		return null
 	return LittleDoodlesEntity.from_request_body(resp["entity"])
@@ -39,7 +73,7 @@ func get_entity(uuid):
 
 func search_entity(search_params):
 	# Returns a list of Entity instances matching the given search parameters
-	var resp = await _send_request("entity/search/?%s" % search_params)
+	var resp = await _send_request(Endpoints.ENTITY_SEARCH % search_params)
 	if resp == null:
 		return null
 
